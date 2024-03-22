f98505726eab119453da2d786942919dd37edc2a
mgiang2015 <mgiang2015@gmail.com>
2024-03-22 18:42:07 +0800

    Initial experimentation

--- my-app/app/server.py
+++ my-app/app/server.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from rag_pinecone import chain as rag_pinecone_chain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+import io
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Blob
+from langchain.document_loaders.parsers import PDFMinerParser
+
+from typing import Annotated
+from fastapi import FastAPI, File, Form, UploadFile
 
 app = FastAPI()
 
@@ -10,6 +19,32 @@ app = FastAPI()
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
+# Route to ingest our resume / project files
+@app.post("/rag-pinecone/files/")
+async def ingest_file(
+        file: bytes = File(...),
+):
+    parser = PDFMinerParser()
+    blob = Blob.from_data (
+        data=file,
+        mime_type="application/pdf",
+    )
+
+    pages = parser.parse(blob=blob)
+
+    all_splits = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+
+    for page in pages:
+        # Split data
+        all_splits += text_splitter.split_documents(page)
+
+    # Add to vectorDB
+    # vectorstore = PineconeVectorStore.from_documents(
+    #     documents=all_splits, embedding=OpenAIEmbeddings(), index_name=PINECONE_INDEX_NAME
+    # )
+
+    return all_splits
 
 # Edit this to add the chain you want to add
 # add_routes(app, NotImplemented)
